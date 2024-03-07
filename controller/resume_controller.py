c5ecd6ac7b384db9e996a25aba43c5cfb8dea5f5
augustinverdier <aug.verdier@gmail.com>
2024-03-07 11:11:03 +0100

    Avancement des class Resume...

--- controller/resume_controller.py
+++ controller/resume_controller.py
@@ -1,3 +1,5 @@
+import json
+
 from view.resume_view import ResumeView
 
 
@@ -37,11 +39,39 @@ class ResumeController:
         except TypeError or ValueError:
             print(f"Votre réponse n'est pas valable, tapez 1, 2, 3, 4, 5 ou 6 {self.general_responses()}")
 
+    @staticmethod
+    def get_player_data():
+
+        with open("../player_data.json", "r") as f:
+            player_dict = f.read()
+            player_data = json.loads(player_dict)
+
+            return player_data
+
     def player_result(self):
         """voir résultats d'un joueur"""
 
+        all_players_name = []
 
-        pass
+        players = self.get_player_data()
+        for row in players:
+            player_name = row["Nom"]
+            all_players_name.append(player_name)
+
+        all_players_name = []
+
+        for row in players:
+            player_name = row["Nom"]
+            all_players_name.append(player_name)
+
+        print("Voici le nom des joueurs :"
+              f"{all_players_name}")
+        user_input = input("Copiez collez le nom du joueur dont vous voulez voir la fiche : ")
+
+        for player in players:
+            if user_input in player.values():
+                return print(f"Voici la fiche du joueur {user_input} - {player}")
+        return print("Ce joueur n'est pas dans notre liste")
 
     def first_round_result(self):
         """voir résultats global du 1er round"""
@@ -61,4 +91,12 @@ class ResumeController:
 
     def ranking_result(self):
         """voir classement de tous les joueurs"""
-        pass
+
+        result = self.get_player_data()
+
+        return sorted(result, key=lambda x: x["Point"])
+
+
+resume_controller = ResumeController()
+# resume_controller.player_result()
+resume_controller.ranking_result()
